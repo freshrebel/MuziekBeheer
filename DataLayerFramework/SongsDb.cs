e1c10d617d91ff9c431933a0fc58bfda3908caa9
Arno Deckx <arno_deckx@hotmail.com>
2017-10-12 16:36:33 +0200

    VariousChanges

---
+++ DataLayerFramework/SongsDb.cs
@@ -0,0 +1,31 @@
+﻿using DataModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayerFramework
+{
+    class SongsDb
+    {
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<SongAlbum> SongAlbums { get; set; }
+        public DbSet<Playlist> Playlists { get; set; }
+        public DbSet<SongPlaylist> SongPlaylists { get; set; }
+        public DbSet<SongGenre> SongGenres { get; set; }
+        public DbSet<SongArtist> SongArtists { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SongAlbum>().HasKey(k => new { k.AlbumId, k.SongId });
+            modelBuilder.Entity<SongPlaylist>().HasKey(k => new { k.SongId, k.PlaylistId });
+            modelBuilder.Entity<SongGenre>().HasKey(k => new { k.SongId, k.GenreId });
+            modelBuilder.Entity<SongArtist>().HasKey(k => new { k.SongId, k.ArtistId });
+        }
+    }
+}
---
+++ MuziekBeheer/Controllers/AlbumsController.cs
@@ -0,0 +1,91 @@
+﻿using DataLayer;
+using System.Web.Mvc;
+
+namespace MuziekBeheer.Controllers
+{
+    public class AlbumsController : Controller
+    {
+
+        SongsDb songsDb = new SongsDb();
+
+        // GET: Albums
+        public ActionResult Index()
+        {
+            var albums = songsDb.Albums;
+
+            return View(albums);
+        }
+
+        // GET: Albums/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Albums/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Albums/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Albums/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Albums/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Albums/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Albums/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
