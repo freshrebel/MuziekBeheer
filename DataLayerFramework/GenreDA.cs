9774d62415a7b19b23f61c5578180caeffd8b383
Arno Deckx <arno_deckx@hotmail.com>
2017-10-27 22:40:02 +0200

    Add GenreDA GenreDA Add functionality

---
+++ DataLayerFramework/GenreDA.cs
@@ -0,0 +1,58 @@
+﻿using DataModelsFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayerFramework
+{
+    public class GenreDA
+    {
+        SongsDb songsDb = new SongsDb();
+
+        public List<Genre> GetAllGenres()
+        {
+            return songsDb.Genres.ToList<Genre>();
+        }
+
+        public Genre GetGenreById(int id)
+        {
+            return songsDb.Genres.Find(id);
+        }
+
+        public Genre GetGenreByName(string name)
+        {
+            var getGenreByNameQuery = from a in songsDb.Genres
+                                      where a.GenreName.ToLower().Trim() == name.ToLower().Trim()
+                                      select a;
+
+            return getGenreByNameQuery.ToList<Genre>()[0];
+        }
+
+        public void AddGenre(Genre genre)
+        {
+            songsDb.Genres.Add(genre);
+            songsDb.SaveChanges();
+        }
+
+        public void EditGenre(Genre genre)
+        {
+            Genre originalGenre = songsDb.Genres.Find(genre.GenreId);
+            songsDb.Entry(originalGenre).CurrentValues.SetValues(genre);
+            songsDb.SaveChanges();
+        }
+
+        public void DeleteGenre(Genre genre)
+        {
+            songsDb.Genres.Remove(genre);
+            songsDb.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            songsDb.Dispose();
+        }
+
+    }
+}
