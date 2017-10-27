d413dde6f0fbc03d06801208fd3444163451a22f
Arno Deckx <arno_deckx@hotmail.com>
2017-10-27 13:34:42 +0200

    add ArtistDA ArtistDA Add functionality part 1

---
+++ DataLayerFramework/ArtistDA.cs
@@ -0,0 +1,55 @@
+﻿using DataModelsFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayerFramework
+{
+    public class ArtistDA
+    {
+        SongsDb songsDb = new SongsDb();
+
+        public List<Artist> GetAllArtists()
+        {
+            return songsDb.Artists.ToList<Artist>();
+        }
+
+        public Artist GetArtistById(int id)
+        {
+            var getArtistByIdQuery = from a in songsDb.Artists.Include("SongArtists.Song")
+                                     where a.ArtistId == id
+                                     select a;
+            return getArtistByIdQuery.ToList<Artist>()[0];
+        }
+
+        public Artist GetArtistByName(string name)
+        {
+            var getArtistByNameQuery = from a in songsDb.Artists
+                                       where a.ArtistName.ToLower().Trim() == name.ToLower().Trim()
+                                       select a;
+            bool artistFound = getArtistByNameQuery.Count() != 0;
+            if (artistFound)
+            {
+                return getArtistByNameQuery.ToList<Artist>()[0];
+            }
+            else
+            {
+                return new Artist();
+            }
+        }
+
+        public void AddArtist(Artist artist)
+        {
+            songsDb.Artists.Add(artist);
+            songsDb.SaveChanges();
+        }
+
+        public void EditArtist(Artist artist)
+        {
+            songsDb.Entry(artist).State = System.Data.Entity.EntityState.Modified;
+            songsDb.SaveChanges();
+        }
+    }
+}
